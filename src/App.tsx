5f39c14ebc66ed85044eff5b1a54fec5f6dd922a
Sayako Hazuki <amf.kano@gmail.com>
2024-06-26 13:54:13 +0800

    Adds format customization

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,83 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { formatDate } from "./utils";
+import { FaGear } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
 
 function App() {
+    const [settingsOpen, setSettingsOpen] = React.useState(false);
+    const toggleSettings = () => setSettingsOpen(!settingsOpen);
+    const closeSettings = () => setSettingsOpen(false);
+
     const [time, setTime] = React.useState(
         new Date().toLocaleTimeString("en-US")
     );
     const [date, setDate] = React.useState(
         new Date().toLocaleDateString("en-US")
     );
-    const [timeFormat, setTimeFormat] = React.useState("h:mm:ss");
+    const [timeFormat, _setTimeFormat] = React.useState("h:mm:ss");
     const [dateFormat, setDateFormat] = React.useState("dddd, MMMM d, yyyy");
 
-    setInterval(() => {
-        const date = new Date();
-        setTime(formatDate(date, timeFormat));
-        setDate(formatDate(date, dateFormat));
-    }, 1);
+    const setTimeFormat = (format: string) => {
+        _setTimeFormat(format);
+        console.log("Setting time format to", format);
+        localStorage.setItem("timeFormat", format);
+    };
+    useEffect(() => {
+        const interal = setInterval(() => {
+            const date = new Date();
+            // console.log(timeFormat);
+            setTime(formatDate(date, timeFormat));
+            setDate(formatDate(date, dateFormat));
+        }, 1);
+        return () => clearInterval(interal);
+    });
 
     return (
         <div className="App">
-            <div className="Container">
+            <div className="SettingsToggle" onClick={toggleSettings}>
+                <FaGear />
+            </div>
+            <div className="Container" onClick={closeSettings}>
                 <div className="Clock">{time}</div>
                 <div className="Date">{date}</div>
+            </div>
+            <div
+                className="SettingsModal"
+                style={{
+                    display: settingsOpen ? "block" : "none",
+                }}
+            >
+                <div className="CloseButton" onClick={toggleSettings}>
+                    <FaTimes />
+                </div>
+
+                {/* Time */}
+                <label className="Label" htmlFor="timeFormat">
+                    Time Format
+                </label>
+                <input
+                    className="TextInput"
+                    type="text"
+                    id="timeFormat"
+                    value={timeFormat}
+                    onChange={(e) => setTimeFormat(e.target.value)}
+                />
+
+                {/* Date */}
+
+                <label className="Label" htmlFor="dateFormat">
+                    Date Format
+                </label>
+                <input
+                    className="TextInput"
+                    type="text"
+                    id="dateFormat"
+                    value={dateFormat}
+                    onChange={(e) => setDateFormat(e.target.value)}
+                />
             </div>
         </div>
     );
